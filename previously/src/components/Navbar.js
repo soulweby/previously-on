c0410cc77025322ef85f308e3be3f0af4db43c48
akhenatix <souleymane.epitech.eu>
2022-05-29 19:53:30 +0200

    starting navbar step

--- previously/src/components/Navbar.js
+++ previously/src/components/Navbar.js
@@ -6,6 +6,7 @@ export default function Navbar() {
     <div className='navbar'>
     <nav className='navlink'>
         <Link to="./">Discover</Link>
+        <form ></form>
     </nav>
 
     </div>
