866dd59e80d2a332c2c95f8f031687456b4ce847
akhenatix <souleymane.epitech.eu>
2022-05-29 22:11:22 +0200

    navbar step done

--- previously/src/components/Navbar.js
+++ previously/src/components/Navbar.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from "../assets/betaseries_random.png"
+
 
 export default function Navbar() {
   return (
     <div className='navbar'>
     <nav className='navlink'>
-        <Link to="./">Discover</Link>
-        <form ></form>
+        <div className='main-logo'>
+        <img src={logo} alt='logo' className='logo' />
+        <Link to="./" className='link'>Discover</Link>
+        <Link to="./favorite" className='link'>Favorite</Link>
+        </div>
+        <form className='navform'>
+          <input  type="search" placeholder="search series"/>
+          <input  type="submit" value="recherche"/>
+        </form>
     </nav>
 
     </div>
