85b7720e75a0a5b52eb1a3a2f181b19eacd9aa76
akhenatix <souleymane.epitech.eu>
2022-06-02 01:25:43 +0200

    styling genre adding hometop components

---
+++ previously/src/components/Card.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function Card({movie}) {
+  return (
+    <div className='card'>
+    <h2>{movie.title}</h2>
+    <img src={ movie?.images.poster} alt="poster" />
+    <p>Genres: {movie.genres.Drama}  {" "}
+                        {movie.genres.Crime} {" "}
+                        {movie.genres.Horror} {" "}
+                        {movie.genres.Anime} {" "}
+                        {movie.genres.Adventure} {" "}
+                        {movie.genres.Fantasy} {" "}
+                        {movie.genres.Animation} {" "}
+                        {movie.genres.Science_Fiction} {" "}
+                        {movie.genres.Romance} {" "}
+                        {movie.genres.Documentary} {" "}
+                        {movie.genres.Reality} {" "}
+                        {movie.genres.Western} {" "}
+                        {movie.genres.Talk_Show} {" "}
+                        {movie.genres.Game_Show} {" "}
+                        {movie.genres.Thriller} {" "}
+                        {movie.genres.Children} {" "}
+                        {movie.genres.Family} {" "}
+                        {movie.genres.Home_and_Garden} {" "}
+                        {movie.genres.Mystery} {" "}
+                        {movie.genres.Special_Interest} {" "}
+                        {movie.genres.Sport} {" "}
+                        {movie.genres.Suspense} {" "}
+                        {movie.genres.Travel} {" "}
+                        {movie.genres.History} {" "}
+                        {movie.genres.Indie} {" "}
+                        {movie.genres.Musical} {" "}
+                        {movie.genres.Podcast} {" "}
+                        {movie.genres.War} {" "}
+                        {movie.genres.Martial_Arts} {" "}
+                  
+    </p>
+    </div>
+  )
+}
--- previously/src/components/Discover.js
+++ previously/src/components/Discover.js
@@ -1,29 +1,43 @@
 import React from 'react';
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import api from "../api"
+import Card from './Card';
+import HomeTop from './HomeTop';
 
 
 export default function Discover() {
 
-    //Clé API :2df01f59dc89
-    //Clé secrète (pour OAuth 2.0) : 
-    // "access_token": "02c0567faad4" 
+  //Clé API :2df01f59dc89
+  //Clé secrète (pour OAuth 2.0) : 
+  // "access_token": "02c0567faad4" 
 
-    const [movie, setmovie] = useState([]);
-    useEffect(() => {
-       const fetchData = async () => {
-           let result = await api.get("https://api.betaseries.com/shows/discover")
-          //  console.log(result.data.shows);
-          //  let finalResult = result.data.shows;
+  const [movies, setmovie] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      let result = await api.get("https://api.betaseries.com/shows/discover")
+      console.log(result.data.shows);
+      let finalResult = result.data.shows;
 
-          //  setmovie(finalResult)
-       }
+      setmovie(finalResult)
+    }
 
-       fetchData();
-    }, []);
+    fetchData();
+  }, []);
   return (
+
     <div>
- 
+
+    <h2 className='btn'>les series a decouvrir</h2>
+      <div className='result'>
+        {movies.map((movie) => (
+          <Card key={movie.id} movie={movie} />
+        ))}
+      </div>
+      <div className='home-top'>
+      {movies.map((top) => (
+          <HomeTop key={top.id} top={top} />
+        ))}
+      </div>
     </div>
   )
 }
